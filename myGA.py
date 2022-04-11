04761e0c541caa7cfb826476c076fa58e39bf6d7
walidmzough <walidmzough@gmail.com>
2022-04-11 14:19:34 +0000

    Added binToFloat & still working on mutation

--- myGA.py
+++ myGA.py
@@ -3,6 +3,34 @@ import random as rn
 import matplotlib.pyplot as plt
 import numpy as np
 
+def floatToBin(number):
+    binaryNum = []
+    whole, dec = str(number).split(".")
+    whole, dec = int(whole),int(dec)
+    whole, dec = bin(whole).lstrip("0b"),bin(dec).lstrip("0b")
+    
+    listwhole=[]
+    listwhole[:0]=whole
+    listdec=[]
+    listdec[:0]=dec
+    return [listwhole,listdec]
+
+
+def binTofloat(l):
+    n=0
+    m=0
+    i=len(l[0])
+    j=len(l[1])
+    for k in l[0]:
+        n+=k*2**(i-1)
+        i-=1
+    for k in l[1]:
+        m+=k*2**(j-1)
+        j-=1
+    n+=m/10**(len(str(m)))
+    return n
+
+
 population = np.linspace(2,4,100)
 
 
@@ -15,50 +43,53 @@ def fitness(x):
 def fitnesscolculation(pop):
     fitnesses =[]
     for item in pop:
-        fitnesses.append((item,fitness(item)))
+        fitnesses.append(item)
     return fitnesses 
     
 
 
 def selection(fitnesses) :
     rouletteRusse = []
+    resultat =[]
     for i in range(len(fitnesses)):
-        for j in range(floor(fitnesses[i])):
-            rouletteRusse.append(pop[i])
+        for j in range(floor(fitnesses[1])):
+            rouletteRusse.append(fitnesses[i])
     for i in range(50):
         choice=rn.randint(0,len(rouletteRusse)-1)
-        besties.append(rouletteRusse[choice])
-        besties.sort()
-    return besties
+        resultat.append(rouletteRusse[choice])
+        #resultat.sort()
+    return resultat
 
-def newgeneration(pop, besti):
-    pop = []
+def newGeneration(gen):
+    newGen = []
     for i in range(1):
-        pop.append(besti[i])
-    for i in range(2,len(besti)-1):
-        for j in range(3,len(besti)):
-            newChild = crossover(besti[i],besti[j])
-            pop.append(newChild[0])
-            pop.append(newChild[1])
-    return pop
+        newGen.append(gen[i])
+    for i in range(2,len(gen)-1,2):
+        for j in range(3,len(gen),2):
+            newChild = crossover(gen[i],gen[j])
+            newGen.append(newChild[0])
+            newGen.append(newChild[1])
+    
+    return newGen
 
 def crossover(x,y):
-    if rn.randint(1,10)<=4:
-        x,y = floatToBin(x),floatToBin(y)
-        crossOverPoint = rn.randint(1, min(len(x),len(y)))    
-        newX, newY = x[:crossOverPoint].append(y[crossOverPoint:]),y[:crossOverPoint].append(x[crossOverPoint:])
-    else: newX, newY= x,y
-    return newX, newY
+    x,y= floatToBin(x),floatToBin(y)
+    print(x,y)
+    if rn.randint(1,10)<=12:
+        crossOverPoint = rn.randint(1, min(len(x[0]),len(y[0])))
+        print(crossOverPoint)
+        x[0],y[0] = x[0][:crossOverPoint]+y[0][crossOverPoint:],y[0][:crossOverPoint]+x[0][crossOverPoint:]
+    return [x,y]
+
 
 def mutation(x):
     if rn.randint(1,10)<=7:
-        if len(x) < 4:
-            if x[0] == '0': x[0]='1'
-            else: x[0] = '0'
+        m=rn.randint(0,len(x[1]))
+        if ([x][1][m] ==1) :
+            globals()[x] = [1][m]=0
         else :
-            if x[-4] == '0': x[-4]='1'
-            else: x[-4] = '0'
-    return x
+            globals()[x] = [1][m]=1
+
 
 
 def GA():
@@ -67,7 +98,7 @@ def GA():
     while a !=0 :
         fitnesscolculation(population)
         selection(population)
-        newgeneration()
+        newGeneration()
         population = population[0:2] + [mutation(k) for k in population[2:]]
         print(population)
 
@@ -77,22 +108,5 @@ def GA():
 
 
 
-
-
-def floatToBin(number):
-    binaryNum = []
-    whole, dec = str(number).split(".")
-    whole, dec = int(whole),int(dec)
-    whole, dec = bin(whole).lstrip("0b"),bin(dec).lstrip("0b")
-    binaryNum = [k for k in whole]
-    binaryNum.append('.')
-    for e in dec:
-        binaryNum.append(e) 
-    listwhole=[]
-    listwhole[:0]=whole
-    listdec=[]
-    listdec[:0]=dec
-    return binaryNum, listwhole, listdec
-
-GA()
+#GA()
 
--- test.py
+++ test.py
@@ -1,28 +1,11 @@
 import random as rn
-
-def crossover(x,y):
-    x,y= floatToBin(x),floatToBin(y)
-    print(x,y)
-    if rn.randint(1,10)<=12:
-        crossOverPoint = rn.randint(1, min(len(x[0]),len(y[0])))
-        print(crossOverPoint)
-        x[0],y[0] = x[0][:crossOverPoint]+y[0][crossOverPoint:],y[0][:crossOverPoint]+x[0][crossOverPoint:]
-    return [x,y]
-
-
-
-
-
-def floatToBin(number):
-    binaryNum = []
-    whole, dec = str(number).split(".")
-    whole, dec = int(whole),int(dec)
-    whole, dec = bin(whole).lstrip("0b"),bin(dec).lstrip("0b")
-    
-    listwhole=[]
-    listwhole[:0]=whole
-    listdec=[]
-    listdec[:0]=dec
-    return [listwhole,listdec]
-
-print (crossover(125.36,36.20))
+x=[0,[1,1,1,1,1,1]]
+def mutation(x):
+    if rn.randint(1,10)<=7:
+        m=rn.randint(0,len(x[1])-1)
+        if (x[1][m] ==1) :
+            globals()[x] = [1][m]=0
+        else :
+            globals()[x] = [1][m]=1
+mutation(x)
+print(x)
