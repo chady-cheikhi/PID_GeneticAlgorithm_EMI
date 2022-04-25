1620eae3e68cbac600f009f9533192fb7a409469
walidmzough <walidmzough@gmail.com>
2022-04-25 10:23:26 +0000

    Establishing imperative code

--- myGA.py
+++ myGA.py
@@ -1,112 +1,130 @@
-from math import floor
+from math import floor,sin,cos
 import random as rn
 import matplotlib.pyplot as plt
 import numpy as np
 
-def floatToBin(number):
-    binaryNum = []
-    whole, dec = str(number).split(".")
-    whole, dec = int(whole),int(dec)
-    whole, dec = bin(whole).lstrip("0b"),bin(dec).lstrip("0b")
-    
-    listwhole=[]
-    listwhole[:0]=whole
-    listdec=[]
-    listdec[:0]=dec
-    return [listwhole,listdec]
-
+#define global variables
+pop = []
+newpop=[]
+a = 2
+b = 3
+#utilities
+def generatePop():
+    global pop
+    radical = []
+    for i in range (0,2) :
+        for j in range (0,2) :
+            for k in range (0,2) :
+                for l in range (0,2) :
+                    radical.append(str(i)+str(j)+str(k)+str(l))
+    for i in radical :
+        for j in radical :
+            pop.append([i,j])
+    while(len(pop)!=10):
+        pop.pop(rn.randint(0,len(pop)-1))
 
 def binTofloat(l):
     n=0
-    m=0
-    i=len(l[0])
-    j=len(l[1])
+    i=4
     for k in l[0]:
-        n+=k*2**(i-1)
+        n+=int(k)*2**(i-1)
         i-=1
     for k in l[1]:
-        m+=k*2**(j-1)
-        j-=1
-    n+=m/10**(len(str(m)))
-    return n
+        i-=1
+        n+=int(k)*2**(i)     
+    return a+n/16*(b-a)
 
-
-population = np.linspace(2,4,100)
-
-
+#function to minimise
 def function(x):
-    return (x-3)**2 + 2
+    return sin(-x**2)
 
+def functionForBin(x):
+    return function(binTofloat(x))
+
+
+#fitness function
 def fitness(x):
-    return function(x)
+    return floor(function(binTofloat(x))*10)
 
-def fitnesscolculation(pop):
-    fitnesses =[]
-    for item in pop:
-        fitnesses.append(item)
-    return fitnesses 
-    
+#ellitism
+def selectElite():
+    global newpop
+    global pop
+    acc = pop
+    for j in range(0,2):
+        max = 0
+        for i in range(len(acc)) :
+            if functionForBin(acc[i]) > functionForBin(acc[max]) :
+                max =i
+        newpop.append(acc[max])
+        acc.pop(max)
 
-
-def selection(fitnesses) :
+#selection process
+def selection() :
+    global newpop
+    global pop
     rouletteRusse = []
-    resultat =[]
-    for i in range(len(fitnesses)):
-        for j in range(floor(fitnesses[1])):
-            rouletteRusse.append(fitnesses[i])
-    for i in range(50):
+    resultat = []
+    for i in pop:
+        for j in range(fitness(i)):
+            rouletteRusse.append(i)
+    for i in range(8):
         choice=rn.randint(0,len(rouletteRusse)-1)
         resultat.append(rouletteRusse[choice])
-        #resultat.sort()
-    return resultat
+    pop = resultat
 
-def newGeneration(gen):
-    newGen = []
-    for i in range(1):
-        newGen.append(gen[i])
-    for i in range(2,len(gen)-1,2):
-        for j in range(3,len(gen),2):
-            newChild = crossover(gen[i],gen[j])
-            newGen.append(newChild[0])
-            newGen.append(newChild[1])
-    
-    return newGen
-
-def crossover(x,y):
-    x,y= floatToBin(x),floatToBin(y)
-    print(x,y)
-    if rn.randint(1,10)<=12:
-        crossOverPoint = rn.randint(1, min(len(x[0]),len(y[0])))
-        print(crossOverPoint)
-        x[0],y[0] = x[0][:crossOverPoint]+y[0][crossOverPoint:],y[0][:crossOverPoint]+x[0][crossOverPoint:]
-    return [x,y]
-
-
-def mutation(x):
+#crossover
+def crossover():
+    x=rn.randint(0,7)
+    y=rn.randint(0,7)
+    global newpop
+    global pop
     if rn.randint(1,10)<=7:
-        m=rn.randint(0,len(x[1]))
-        if ([x][1][m] ==1) :
-            globals()[x] = [1][m]=0
-        else :
-            globals()[x] = [1][m]=1
+        crossOverPoint = rn.randint(0,3)
+        newpop.append([pop[x][0][:crossOverPoint]+pop[y][0][crossOverPoint:],pop[x][1]])
+        newpop.append([pop[y][0][:crossOverPoint]+pop[x][0][crossOverPoint:],pop[y][1]])
 
 
-
-def GA():
-    population = np.linspace(2,4,100)
-    a=999
-    while a !=0 :
-        fitnesscolculation(population)
-        selection(population)
-        newGeneration()
-        population = population[0:2] + [mutation(k) for k in population[2:]]
-        print(population)
-
-
-
-
-
-
-
-#GA()
-
+#mutation
+def mutation():
+    global newpop
+    b=rn.randint(1,4)
+    for i in newpop :
+        s=""
+        if rn.randint(1,10)<=7:
+            for j in range(4):
+                if j==b :
+                    s+= "0" if i[1][b]=="1" else "1"
+                else :
+                    s+=i[1][j]
+            i[1]=s
+    
+#execution
+iteration=1
+#First We generate a population
+print("Generation population...")
+generatePop()
+#Start Genetic Algorithm Loop
+while (iteration < 50) :
+    print("Processing Generation Number "+str(iteration)+" :")
+    print("====================================")
+    #We go through selection
+    print("Selecting...")
+    selection()
+    #we choose two random entites to be crossovered with a 70% probability till newpop is full
+    while (len(newpop)!=10) :
+        crossover()
+    print("Crossover is Over")
+    #then we mutate with a 70% probability
+    print("Mutating...")
+    mutation()
+    #we apply ellitism
+    print("Promoting the elite")
+    selectElite()
+    #we make new pop the actual pop
+    newpop.sort(key=functionForBin,reverse=True)
+    pop=newpop
+    print("Generation Number "+str(iteration)+" is the following:")
+    print([binTofloat(i) for i in pop])
+    newpop=[]
+    iteration+=1
